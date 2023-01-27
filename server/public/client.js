ed54d901a325b2307ead8f75c05ebdc20176e293
Solomon Lambert <sol.lambert@gmail.com>
2023-01-26 20:17:26 -0600

    get requests from client and pass constructed render string to render

--- server/public/client.js
+++ server/public/client.js
@@ -3,5 +3,28 @@ $(() => {
 });
 
 function onReady() {
+        getHistory();
+}
+
+function getHistory() {
+        renderHistory(slay.get('/history'));
+}
+
+function renderHistory(promise) {
+        promise.then((res) => {
+                let history = res;
+                let renderString = '';
+                console.log(history);
+                for (let i in history) {
+                        renderString += `
+                        <div id='history-${i}'>
+                          <p>${history[i]}</p>
+                        </div>`
+                }
+                render('#div-history', renderString);
+        })
+}
+
+function render(id, string, className) {
 
 }
