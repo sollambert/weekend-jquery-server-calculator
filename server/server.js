3e043a647094664bd84e45d7d8536b4f1429ec0d
Solomon Lambert <sol.lambert@gmail.com>
2023-01-26 20:03:02 -0600

    created functionality for recieving GET requests on /history and returning history array

--- server/server.js
+++ server/server.js
@@ -5,3 +5,17 @@ app.use(express.static('server/public'));
 app.use(express.urlencoded({extended: true}));
 
 const PORT = 8000;
+
+//Collection of strings representing past mathematical expressions
+const history = [];
+
+/**
+ * Sends history array as payload when recieving a GET on /history
+ */
+app.get('/history', (req, res) => {
+        res.send(history);
+});
+
+app.listen(PORT, () => {
+
+});
