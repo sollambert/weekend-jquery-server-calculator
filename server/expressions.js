3f5324800c2638cf6d5079733552528d4df028bc
Solomon Lambert <sol.lambert@gmail.com>
2023-01-28 01:17:14 -0600

    move expression functions from server.js to expressions.js

---
+++ server/expressions.js
@@ -0,0 +1,217 @@
+//array of accepted operators
+const ops = ['+', '-', '/', '*', '^'];
+
+//regex for splitting array by operator
+const opsRegex = new RegExp("(?<=[-+*/^])|(?=[-+*/^])");
+
+const expPrefix = 'Expressions:'
+
+//function for splitting array into operator objects and check for negative values following operators
+function refactor(arr, expString) {
+        for (let i in arr) {
+                if (evalOp(arr[i])) {
+                        arr[i] = opToObject(arr[i]);
+                        if ((i != 0 && arr[i].op == '-' && ops.includes(arr[i - 1].op))
+                        || i == 0 && arr[i].op == '-') {
+                                arr[i] = arr[i].op + arr[Number(i) + 1];
+                                arr.splice(Number(i) + 1, 1);
+                                i--;
+                                expString += arr[i+1];
+                        } else {
+                                expString += arr[i].op;
+                        }
+                }else {
+
+                        expString += arr[i];
+                }
+        }
+        return {arr, expString};
+}
+
+//function to begin evaluation of expression split into array an array containing operands and operator objects
+function evalExp(arr) {
+        let weight = 3;
+        let result = 0;
+        while (weight > 0) {
+                let weightedResult = evalWeightedExp(arr, weight);
+                if (weightedResult != 0) {
+                        result = weightedResult;
+                }
+                weight--;
+        }
+        return result;
+}
+
+//function to evaluate all expressions by weighted value according to order of operation
+function evalWeightedExp(arr, weight) {
+        let result = 0;
+        for (let i = 0; i < arr.length; i++) {
+                if (arr[i].wgt == weight) {
+                        arr[i - 1] = arr[i].func([arr[i - 1], arr[Number(i) + 1]]);
+                        arr.splice(i, 2);
+                        i -= 2;
+                        result = arr[i + 1];
+                }
+        }
+        return result;
+}
+
+//function for getting associated function of char representing operator
+function evalOp(operator) {
+        switch (operator) {
+                case '+':
+                        return add;
+                case '-':
+                        return sub;
+                case '*':
+                        return mult;
+                case '/':
+                        return div;
+                case '^':
+                        return exp;
+        }
+        return;
+}
+
+//recieves char representing operator
+//returns an object containing operator character 'op', associated function 'func' and weight according to order of operations
+function opToObject(op) {
+        let wgt = 0;
+        switch (op) {
+                case '^':
+                        wgt = 3;
+                        break;
+                case '*':
+                        wgt = 2;
+                        break;
+                case '/':
+                        wgt = 2;
+                        break;
+                case '+':
+                        wgt = 1;
+                        break;
+                case '-':
+                        wgt = 1;
+        }
+        return { op, func: evalOp(op), wgt }
+}
+
+//function for addition of an array of numbers
+//returns total sum
+function add(nums) {
+        let result = Number(nums[0]);
+        for (let i = 1; i < nums.length; i++) {
+                console.log(expPrefix, `adding ${result} to ${nums[i]}`)
+                result += Number(nums[i]);
+        }
+        console.log(expPrefix, 'result', result);
+        return result;
+}
+
+//function for subtraction of an array of numbers
+//returns total subtraction of elements of array starting with the first value
+function sub(nums) {
+        let result = Number(nums[0]);
+        for (let i = 1; i < nums.length; i++) {
+                console.log(expPrefix, `subbing ${result} to ${nums[i]}`)
+                result -= Number(nums[i]);
+        }
+        console.log(expPrefix, 'result', result);
+        return result;
+}
+
+//function for multiplication of an array of numbers
+//returns total multiplication of elements of array starting with the first value
+function mult(nums) {
+        let result = Number(nums[0]);
+        for (let i = 1; i < nums.length; i++) {
+                console.log(expPrefix, `mult ${result} to ${nums[i]}`)
+                result *= Number(nums[i]);
+        }
+        console.log(expPrefix, 'result', result);
+        return result;
+}
+
+//function for division of an array of numbers
+//returns total division of elements of array starting with the first value
+function div(nums) {
+        let result = Number(nums[0]);
+        for (let i = 1; i < nums.length; i++) {
+                console.log(expPrefix, `div ${result} to ${nums[i]}`)
+                result /= Number(nums[i]);
+        }
+        console.log(expPrefix, 'result', result);
+        return result;
+}
+
+//function for exponentation of an array of numbers
+//returns total exponentation of elements of array starting with the first value
+function exp(nums) {
+        let result = Number(nums[0]);
+        for (let i = 1; i < nums.length; i++) {
+                console.log(expPrefix, `exp ${result} to pow ${nums[i]}`)
+                result **= Number(nums[i]);
+        }
+        console.log(expPrefix, 'result', result);
+        return result;
+}
+
+//function for checking validity of a string expression
+//returns true or false depending on validity of input
+function checkCleanInput(exp) {
+        for (let i = 0; i < exp.length; i++) {
+                if (ops.includes(exp[i]) && ops.includes(exp[i - 1]) && exp[i] != '-') {
+                        console.log(expPrefix, 'Input rejected for 2 consecutive operators (non-negative)');
+                        return false;
+                } else if (exp[i] == '-'
+                && exp[i-1] == '-'
+                && exp[i-2] == '-') {
+                        console.log(expPrefix, 'Input rejected for >2 consecutive \'-\' operators');
+                        return false;
+                } else if (exp.match(/[A-Za-z]/)) {
+                        console.log(expPrefix, 'Input rejected for non-numeric characters');
+                        return false;
+                }
+        }
+        return true;
+}
+
+//splits array by regex that looks for operators and maintains integrity of non-operator strings within array
+function splitByOps(arr){
+        return arr.split(opsRegex);
+}
+
+/*function recurParenthesis(arr) {
+        let result = 0;
+        let lastParen
+        console.log(arr);
+        for (let i = 0; i < arr.length; i ++) {
+                if (arr[i] == '(') {
+                        recurParenthesis(arr.splice(i, arr.length - i))
+                }
+        }
+        for (let i = 0; i < arr.length; i ++) {
+                if (arr[i] == ')') {
+                        result = evalExp(arr.slice(arr.slice(0,i-1).lastIndexOf('('), i));
+                }
+        }
+        return result;
+}
+
+function recurParenthesis(arr, index) {
+        let result = 0;
+        console.log(arr);
+        for (let i = 0; i < arr.length; i ++) {
+                if (arr[i] == '(') {
+                recurParenthesis(arr.splice(i, arr.length - i))
+                }
+        }
+        for (let i = 0; i < arr.length; i ++) {
+                if (arr[i] == ')') {
+                        result = evalExp(arr.splice(arr.lastIndexOf('('), i));
+                }
+        }
+        return result;
+}*/
+
+module.exports = {evalExp, refactor, clean: checkCleanInput, splitByOps}
--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
+const e = require('express');
 const express = require('express');
+const expressions = require('./expressions.js')
 const app = express();
 
 app.use(express.static('server/public'));
 app.use(express.urlencoded({ extended: true }));
-
-let ops = ['+', '-', '/', '*', '^'];
-
-let opsRegex = new RegExp("(?<=[-+*/^])|(?=[-+*/^])");
 
 let expString = '';
 
@@ -23,213 +21,28 @@ app.get('/history', (req, res) => {
 });
 
 app.post('/eval', (req, res) => {
-        console.log(req.body.expression);
-        let cleanInput = checkCleanInput(req.body.expression);
-        console.log(cleanInput);
+        let expression = req.body.expression;
+        console.log(`Recieved expression: ${expression}`)
+        let cleanInput = expressions.clean(expression);
         if (cleanInput) {
-                let splitExp = req.body.expression.split(opsRegex);
-                //console.log(splitExp);
-                let result = 0;
-                splitExp = refactorToOpsAndRands(splitExp);
-
-                result = evalExp(splitExp);
-                //console.log(result);
+                let splitExp = expressions.splitByOps(expression);
+                let refactors = expressions.refactor(splitExp, expString);
+                splitExp = refactors.arr;
+                expString = refactors.expString;
+                let result = expressions.evalExp(splitExp).toFixed(5);
                 expString += `=${result}`;
                 history.push(expString);
                 res.send(`${result}`);
-                expString = '';
+                initializeExpString();
         } else {
                 res.sendStatus(400);
         }
 });
 
-function refactorToOpsAndRands(arr) {
-        for (let i in arr) {
-                if (evalOp(arr[i])) {
-                        arr[i] = opToObject(arr[i]);
-                        //console.log(arr);
-                        if ((i != 0 && arr[i].op == '-' && ops.includes(arr[i - 1].op))
-                        || i == 0 && arr[i].op == '-') {
-                                arr[i] = arr[i].op + arr[Number(i) + 1];
-                                console.log(arr);
-                                arr.splice(Number(i) + 1, 1);
-                                i--;
-                                expString += arr[i+1];
-                        } else {
-                                expString += arr[i].op;
-                        }
-                }else {
-
-                        expString += arr[i];
-                }
-        }
-        return arr;
+function initializeExpString() {
+        expString = '';
 }
 
 app.listen(PORT, () => {
 
 });
-
-/*function recurParenthesis(arr) {
-        let result = 0;
-        let lastParen
-        console.log(arr);
-        for (let i = 0; i < arr.length; i ++) {
-                if (arr[i] == '(') {
-                        recurParenthesis(arr.splice(i, arr.length - i))
-                }
-        }
-        for (let i = 0; i < arr.length; i ++) {
-                if (arr[i] == ')') {
-                        result = evalExp(arr.slice(arr.slice(0,i-1).lastIndexOf('('), i));
-                }
-        }
-        return result;
-}
-
-function recurParenthesis(arr, index) {
-        let result = 0;
-        console.log(arr);
-        for (let i = 0; i < arr.length; i ++) {
-                if (arr[i] == '(') {
-                recurParenthesis(arr.splice(i, arr.length - i))
-                }
-        }
-        for (let i = 0; i < arr.length; i ++) {
-                if (arr[i] == ')') {
-                        result = evalExp(arr.splice(arr.lastIndexOf('('), i));
-                }
-        }
-        return result;
-}*/
-
-function checkCleanInput(exp) {
-        for (let i = 0; i < exp.length; i++) {
-                if (ops.includes(exp[i]) && ops.includes(exp[i - 1]) && exp[i] != '-') {
-                        return false;
-                } else if (exp[i] == '-'
-                && exp[i-1] == '-'
-                && exp[i-2] == '-') {
-                        return false;
-                } else if (exp.match(/[A-Za-z]/) != undefined) {
-                        return false;
-                }
-        }
-        return true;
-}
-
-
-function evalExp(arr) {
-        console.log(arr);
-        let weight = 3;
-        let result = 0;
-        while (weight > 0) {
-                let weightedResult = evalWeightedExp(arr, weight);
-                if (weightedResult != 0) {
-                        result = weightedResult;
-                }
-                weight--;
-        }
-        return result;
-}
-
-function evalWeightedExp(arr, weight) {
-        let result = 0;
-        for (let i = 0; i < arr.length; i++) {
-                if (arr[i].wgt == weight) {
-                        arr[i - 1] = arr[i].func([arr[i - 1], arr[Number(i) + 1]]);
-                        arr.splice(i, 2);
-                        i -= 2;
-                        result = arr[i + 1];
-                }
-        }
-        return result;
-}
-
-function opToObject(op) {
-        let wgt = 0;
-        switch (op) {
-                case '^':
-                        wgt = 3;
-                        break;
-                case '*':
-                        wgt = 2;
-                        break;
-                case '/':
-                        wgt = 2;
-                        break;
-                case '+':
-                        wgt = 1;
-                        break;
-                case '-':
-                        wgt = 1;
-        }
-        return { op, func: evalOp(op), wgt }
-}
-
-function add(nums) {
-        let result = Number(nums[0]);
-        for (let i = 1; i < nums.length; i++) {
-                console.log(`adding ${result} to ${nums[i]}`)
-                result += Number(nums[i]);
-        }
-        console.log('result', result);
-        return result;
-}
-
-function sub(nums) {
-        let result = Number(nums[0]);
-        for (let i = 1; i < nums.length; i++) {
-                console.log(`subbing ${result} to ${nums[i]}`)
-                result -= Number(nums[i]);
-        }
-        console.log('result', result);
-        return result;
-}
-
-function mult(nums) {
-        let result = Number(nums[0]);
-        for (let i = 1; i < nums.length; i++) {
-                console.log(`mult ${result} to ${nums[i]}`)
-                result *= Number(nums[i]);
-        }
-        console.log('result', result);
-        return result;
-}
-
-function div(nums) {
-        let result = Number(nums[0]);
-        for (let i = 1; i < nums.length; i++) {
-                console.log(`div ${result} to ${nums[i]}`)
-                result /= Number(nums[i]);
-        }
-        console.log('result', result);
-        return result;
-}
-
-function exp(nums) {
-        let result = Number(nums[0]);
-        for (let i = 1; i < nums.length; i++) {
-                console.log(`exp ${result} to pow ${nums[i]}`)
-                result **= Number(nums[i]);
-        }
-        console.log('result', result);
-        return result;
-}
-
-
-function evalOp(operator) {
-        switch (operator) {
-                case '+':
-                        return add;
-                case '-':
-                        return sub;
-                case '*':
-                        return mult;
-                case '/':
-                        return div;
-                case '^':
-                        return exp;
-        }
-        return;
-}
